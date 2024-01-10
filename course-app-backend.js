709a461ced2178e108be82bfb845131c1e436b9f
Avviishek <avishekdastidar22@gmail.com>
2024-01-11 00:05:27 +0530

    ok change

--- course-app-backend.js
+++ course-app-backend.js
@@ -166,4 +166,4 @@ app.get('/users/purchasedCourses', authenticateJwt, async (req, res) => {
   }
 });
 
-app.listen(3000||process.env.PORT, () => console.log('Server running on port 3000'));
+app.listen(3000, () => console.log('Server running on port 3000'));
